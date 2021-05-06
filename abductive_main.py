9427d71f05a750225a3a9b058d3f519e26a61f4c
qkaren <lianhuiqin9@gmail.com>
2021-05-06 11:14:14 -0700

    Add comment to abductive_main.py

--- abductive_main.py
+++ abductive_main.py
@@ -499,6 +499,9 @@ if __name__ == "__main__":
 
     with open(candidate_output, 'w') as fw:
         for r in records:
+            # We use O2<E>O1 as the left context. Yet prepending O2 here is
+            # minor to the performance. See the latest paper (section 4.1)
+            # for more details.
             o1_text = '<|endoftext|>'.join([r['obs2'], r['obs1']])
             o2_text = r['obs2']
 
